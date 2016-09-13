377d32207391252876a51b5555df996a0c805826
shamu <shamu@LEMMY>
2016-09-12 20:20:41 -0400

    added intrument

---
+++ FinancialModels/IInstrument.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialModels
+{
+    public interface IInstrument
+    {
+        string Symbol { get; set; }
+        string Name { get; set; }
+        string CUSIP { get; set; }
+    }
+}
